42f8340a37d403c1d0636bc6b8cfea6e16ff3842
Jan Graßegger <jan@anycook.de>
2014-08-04 19:53:50 +0200

    added unit testing

---
+++ app/src/test/java/de/anycook/app/util/StringToolsTest.java
@@ -0,0 +1,31 @@
+package de.anycook.app.util;
+
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class StringToolsTest extends TestCase {
+
+    @Test
+    public void testFormatAmount() throws Exception {
+
+        assertEquals("2", StringTools.formatAmount("2"));
+        assertEquals("2,3", StringTools.formatAmount("2,3"));
+        assertEquals("2,3", StringTools.formatAmount("2.3"));
+    }
+
+    @Test
+    public void testMergeAmounts() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiplyAmount() throws Exception {
+
+    }
+}
