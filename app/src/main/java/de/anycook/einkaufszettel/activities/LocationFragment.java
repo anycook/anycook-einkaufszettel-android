aedde815e70f4e00f56878c67eaae3d487e15d5d
Jan Graßegger <jan@anycook.de>
2014-10-31 16:45:36 +0100

    closing #15

--- app/src/main/java/de/anycook/einkaufszettel/activities/LocationFragment.java
+++ app/src/main/java/de/anycook/einkaufszettel/activities/LocationFragment.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of anycook Einkaufszettel
+ * Copyright (C) 2014 Jan Graßegger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [http://www.gnu.org/licenses/].
+ */
+
 package de.anycook.einkaufszettel.activities;
 
 import android.app.ListFragment;
@@ -20,7 +38,6 @@ import de.anycook.einkaufszettel.util.GPSTracker;
  * @author Jan Graßegger
  */
 public class LocationFragment extends ListFragment {
-
     private static   String URL_PATTERN = "https://api.anycook.de/discover/near?latitude=%s&" +
             "longitude=%s&maxRadius=50&recipeNumber=20";
 
